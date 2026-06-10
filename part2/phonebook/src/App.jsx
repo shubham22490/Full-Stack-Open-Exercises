9f3bef57b8d5f1d6f590d68d6c2740e44e3edcca
Shubham Goel <shubham22490@iiitd.ac.in>
2026-06-10 08:52:58 +0530

    Done with Part2C, Getting data from the server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  
+  // Getting data from the server using useEffect which gets the data after rendering and rerenders once gets the data.
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(resp => setPersons(resp.data))
+  }
+  useEffect(hook, [])
+  
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filterLabel, setFilterLabel] = useState('')
