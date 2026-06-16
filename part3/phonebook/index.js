eee80b7de2ccec27ab488c61d8173104a37fd89d
Shubham Goel <shubham22490@iiitd.ac.in>
2026-06-17 00:46:17 +0530

    Added frontend static files to the phonebook backend

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,8 +3,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
